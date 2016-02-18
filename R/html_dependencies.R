6640d1c87dbe533fd0b9b77fe38090335a2f028f
Julien Barnier <julien@nozav.org>
2016-02-18 16:22:24 +0100

    Upgrade bootstrap to 3.3.6

--- R/html_dependencies.R
+++ R/html_dependencies.R
@@ -45,14 +45,14 @@ html_dependency_jquery_stickytableheaders <- function()  {
 # create an html dependency for bootstrap (function copied from rmarkdown)
 html_dependency_bootstrap <- function(theme = "bootstrap") {
   htmltools::htmlDependency(name = "bootstrap",
-                 version = "3.3.1",
-                 src = system.file("templates/bootstrap-3.3.1", package = "rmdformats"),
+                 version = "3.3.6",
+                 src = system.file("templates/bootstrap-3.3.6", package = "rmdformats"),
                  meta = list(viewport = "width=device-width, initial-scale=1"),
                  script = c(
-                   "js/bootstrap.min.js",
+                   "js/bootstrap.min.js"
                    # These shims are necessary for IE 8 compatibility
-                   "shim/html5shiv.min.js",
-                   "shim/respond.min.js"
+                   #"shim/html5shiv.min.js",
+                   #"shim/respond.min.js"
                  ),
                  stylesheet = paste("css/", theme, ".min.css", sep = ""))
 }
@@ -60,8 +60,8 @@ html_dependency_bootstrap <- function(theme = "bootstrap") {
 # create an html dependency for bootstrap js only (function copied from rmarkdown)
 html_dependency_bootstrap_js <- function() {
   htmltools::htmlDependency(name = "bootstrap_js",
-                            version = "3.3.1",
-                            src = system.file("templates/bootstrap-3.3.1", package = "rmdformats"),
+                            version = "3.3.6",
+                            src = system.file("templates/bootstrap-3.3.6", package = "rmdformats"),
                             meta = list(viewport = "width=device-width, initial-scale=1"),
                             script = c(
                               "js/bootstrap.min.js"
