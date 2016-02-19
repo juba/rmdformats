e4939b5409644b35c5915010305d3802b64aa28d
Julien Barnier <julien@nozav.org>
2016-02-19 12:08:49 +0100

    JavaScript cleanup

--- inst/templates/readthedown/readthedown.js
+++ inst/templates/readthedown/readthedown.js
@@ -23,9 +23,7 @@ $( document ).ready(function() {
     $(".tab-content").each(function() {
       $(this).find(".tab-pane").first().addClass("active")
     })
-});
 
-$( document ).ready(function() {
     // ScrollSpy also requires that we use a Bootstrap nav component.
     $('#text-table-of-contents ul').first().addClass('nav');
     $('body').scrollspy({target: '#text-table-of-contents'});
@@ -34,33 +32,3 @@ $( document ).ready(function() {
     $('table').stickyTableHeaders();
 
 });
-
-window.SphinxRtdTheme = (function (jquery) {
-    var stickyNav = (function () {
-        var navBar,
-            win,
-            stickyNavCssClass = 'stickynav',
-            applyStickNav = function () {
-                if (navBar.height() <= win.height()) {
-                    navBar.addClass(stickyNavCssClass);
-                } else {
-                    navBar.removeClass(stickyNavCssClass);
-                }
-            },
-            enable = function () {
-                applyStickNav();
-                win.on('resize', applyStickNav);
-            },
-            init = function () {
-                navBar = jquery('nav.wy-nav-side:first');
-                win    = jquery(window);
-            };
-        jquery(init);
-        return {
-            enable : enable
-        };
-    }());
-    return {
-        StickyNav : stickyNav
-    };
-}($));
