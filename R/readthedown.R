e3d9ca5b56c9457af312e541101351dc3299cc4d
Julien Barnier <julien@nozav.org>
2016-03-22 11:51:45 +0100

    Add `lightbox` and `gallery` option to readthedown

--- R/readthedown.R
+++ R/readthedown.R
@@ -9,6 +9,8 @@
 #'   "default", "tango", "pygments", "kate", "monochrome", "espresso",
 #'   "zenburn", "haddock", and "textmate". Pass \code{NULL} to prevent syntax
 #'   highlighting.
+#' @param lightbox if TRUE, display content images as thumbnails with lightbox effect
+#' @param gallery if TRUE and popup is TRUE, add a gallery navigation between images in lightbox display
 #' @param pandoc_args arguments passed to the pandoc_args argument of rmarkdown \code{\link{html_document}}
 #' @param ... Additional function arguments passed to R Markdown \code{\link{html_document}}
 #' @return R Markdown output format to pass to \code{\link{render}}
@@ -21,22 +23,30 @@ readthedown <- function(fig_width = 8,
                        fig_height = 5,
                        fig_caption = TRUE,
                        highlight = "kate",
+                       lightbox = FALSE,
+                       gallery = FALSE,
                        pandoc_args = NULL,
                        ...) {
  
   ## js and css dependencies
   extra_dependencies <- list(html_dependency_jquery(),
                              html_dependency_bootstrap(),
-                             #html_dependency_jquery_stickytableheaders(),
+                             html_dependency_magnific_popup(),
                              html_dependency_readthedown())
   
   ## Force mathjax arguments
   pandoc_args <- c(pandoc_args, 
                    "--mathjax", 
                    "--variable", paste0("mathjax-url:", default_mathjax()))
+  if (lightbox) { pandoc_args <- c(pandoc_args, "--variable", "lightbox:true") }
+  if (gallery) {
+    pandoc_args <- c(pandoc_args, "--variable", "gallery:true")
+  } else {
+    pandoc_args <- c(pandoc_args, "--variable", "gallery:false")
+  }
   
   rmarkdown::html_document(
-      template = system.file("templates/readthedown/default.html", package = "rmdformats"),
+      template = system.file("templates/readthedown/readthedown.html", package = "rmdformats"),
       extra_dependencies = extra_dependencies,
       fig_width = fig_width,
       fig_height = fig_height,
