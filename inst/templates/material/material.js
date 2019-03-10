ea2d99661b576bcd05fa4d84e1c5f8bf55ecf5ff
Julien <julien@nozav.org>
2019-03-10 22:36:23 +0100

    Fix problems in material when there is a dot in level 1 heading. Close #54

--- inst/templates/material/material.js
+++ inst/templates/material/material.js
@@ -1,11 +1,19 @@
 $(function() {
 
+    // Fix for dots in level 1 titles
+    $('body .section.level1').each(function() {
+      $(this).attr("id", $(this).attr("id").replace(/\./g , "-"));
+    });
+    $('#toc ul li a').each(function() {
+      $(this).attr("href", $(this).attr("href").replace(/\./g , "-"));
+    });
+
     /* Menu */
     $('#toc ul').first().addClass('nav');
     $('#toc ul li').addClass('withripple');
     $('#toc ul li').attr("data-target", function() {
         return($(this).children("a").attr("href"));
-    })
+    });
     $('body .section.level1').addClass("well page");
 
     $(window).on("resize", function () {
