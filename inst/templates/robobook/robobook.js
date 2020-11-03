4974f3bf5313919b5b5f985ccbff3f993a81e214
Julien <julien@nozav.org>
2020-11-03 10:23:37 +0100

    Add robobook template.

--- inst/templates/robobook/robobook.js
+++ inst/templates/robobook/robobook.js
@@ -1,26 +1,15 @@
 $( document ).ready(function() {
 
+    if ($(document).width() <= 1000) {
+      $(".book").removeClass("with-summary");
+    }
     // Shift nav in mobile when clicking the menu.
-    $(document).on('click', "[data-toggle='wy-nav-top']", function() {
-      $("[data-toggle='wy-nav-shift']").toggleClass("shift");
+    $(document).on('click', ".toggle-sidebar", function() {
+      $(".book").toggleClass("with-summary");
     });
-    // Close menu when you click a link.
-    $(document).on('click', "#table-of-contents ul li a", function() {
-      $("[data-toggle='wy-nav-shift']").removeClass("shift");
-    });
-    // Close menu when you click on main content
-    $(document).on('click', "#main, #header", function() {
-      $("[data-toggle='wy-nav-shift']").removeClass("shift");
-    });
-
-    // Make tables responsive
-    $("#main table").wrap("<div class='table-wrapper'></div>");
 
     // ScrollSpy also requires that we use a Bootstrap nav component.
-    $('#text-table-of-contents ul').first().addClass('nav');
-    $('body').scrollspy({target: '#text-table-of-contents'});
-
-    // add sticky table headers
-    //$('table').stickyTableHeaders();
+    $('#toc ul').first().addClass('nav');
+    $('.book-body').scrollspy({target: '#toc'});
 
 });
