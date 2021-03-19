eb04d67daf109a1bdad3fe1b05dd50166cbd5957
Julien <julien@nozav.org>
2021-03-19 14:30:12 +0100

    Fix invalid ids when dots in headings.
    Close #99

--- inst/templates/downcute/downcute.js
+++ inst/templates/downcute/downcute.js
@@ -139,6 +139,17 @@ window.addEventListener("load", (event) => {
     // Make tables responsive
     $(".page-content table").wrap("<div class='table-wrapper'></div>");
 
+    // Fix for dots in level 1 and level 2 titles
+    $('body .section.level1').each(function () {
+      $(this).attr("id", $(this).attr("id").replace(/\./g, "-"));
+    });
+    $('body .section.level2').each(function () {
+      $(this).attr("id", $(this).attr("id").replace(/\./g, "-"));
+    });
+    $('#toc ul li a').each(function () {
+      $(this).attr("href", $(this).attr("href").replace(/\./g, "-"));
+    });
+
     // ScrollSpy also requires that we use a Bootstrap nav component.
     $('#toc ul').first().addClass('nav');
     $('body').scrollspy({target: '#toc'});
