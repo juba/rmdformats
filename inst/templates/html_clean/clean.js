260c69cc0d21ce9caac9132769ddbf02434bc74b
Julien <julien@nozav.org>
2021-03-19 17:01:09 +0100

    Fix dot in headings in html_clean

--- inst/templates/html_clean/clean.js
+++ inst/templates/html_clean/clean.js
@@ -1,5 +1,16 @@
 $(function() {
 
+    // Fix for dots in level 1 and level 2 titles
+    $('body .section.level1').each(function () {
+      $(this).attr("id", $(this).attr("id").replace(/\./g, "-"));
+    });
+    $('body .section.level2').each(function () {
+      $(this).attr("id", $(this).attr("id").replace(/\./g, "-"));
+    });
+    $('#toc ul li a').each(function () {
+      $(this).attr("href", $(this).attr("href").replace(/\./g, "-"));
+    });
+
     // ScrollSpy also requires that we use a Bootstrap nav component.
     $('#toc ul').first().addClass('nav');
     $('body').scrollspy({target: '#toc'});
